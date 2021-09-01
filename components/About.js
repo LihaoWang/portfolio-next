e4b09d95fbad8793ba97cf8a309a8b05cc2a2de3
leowang <leowang9863@gmail.com>
2021-09-01 16:53:04 -0400

    updated intro

--- components/About.js
+++ components/About.js
@@ -9,17 +9,17 @@ function About() {
       <div className="content">
         <div className="textBox">
           <p className="about-text">
-            My name is Lihao Wang, a second-year Master's student in Computer
-            Science at NYU Tandon School of Engineering. I had experience in
-            developing web applications using React (Next.js), Firebase, and
-            CSS. I am a self-motivated person and enjoys trying out new
-            technology. In my spare time, I created a Chrome browser extension
-            that helps users manage their time and a bio link platform that
-            enables users to share links on one page easily. Besides the
-            technical aspect, I am also experienced in Web Design and UX Design.
-            I believe great products should be not only functional but also easy
-            to use. My passion for creating aesthetic and intuitive products
-            originates from the desire to impact and help others.
+            Lihao Wang is a second-year Master's student in Computer Science at
+            NYU Tandon School of Engineering. He had experience in developing
+            web applications using React (Next.js), Firebase, and CSS. Lihao is
+            a self-motivated person and enjoys trying out new technology. In his
+            spare time, he created a Chrome browser extension that helps users
+            manage their time and a bio link platform that enables users to
+            share links on one page easily. Besides the technical aspect, he is
+            also experienced in Web Design and UX Design. He believes great
+            products should be not only functional but also easy to use. His
+            passion for creating aesthetic and intuitive products originates
+            from the desire to impact and help others.
           </p>
         </div>
         <div style={{ maxWidth: "800px" }} className="imgBx">
